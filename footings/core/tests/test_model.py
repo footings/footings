ca8989b93100616b402c4016275911b68a1845ac
Dustin Tindall <dustin.tindall@gmail.com>
2019-11-15 05:39:33 -0600

    reformatted test

--- footings/core/tests/test_model.py
+++ footings/core/tests/test_model.py
@@ -60,100 +60,112 @@ def calcs():
     def calc_pv(disc_cash: Column("float")) -> CReturn({"pv": "float"}):
         return cumsum(disc_cash)
 
-    return calc_v, calc_v_mode, calc_disc_factor, calc_disc_cash, calc_pv
+    return {
+        "calc_v": calc_v,
+        "calc_v_mode": calc_v_mode,
+        "calc_disc_factor": calc_disc_factor,
+        "calc_disc_cash": calc_disc_cash,
+        "calc_pv": calc_pv,
+    }
 
 
 @pytest.fixture(scope="module")
 def registry(calcs):
-    calc_v, calc_v_mode, calc_disc_factor, calc_disc_cash, calc_pv = calcs
+
     registry_base = Registry(
-        as_calculation(calc_v),
-        as_calculation(calc_disc_factor),
-        as_calculation(calc_disc_cash),
-        as_calculation(calc_pv),
+        as_calculation(calcs["calc_v"]),
+        as_calculation(calcs["calc_disc_factor"]),
+        as_calculation(calcs["calc_disc_cash"]),
+        as_calculation(calcs["calc_pv"]),
     )
 
     registry_mode = Registry(
-        as_calculation(calc_v_mode),
-        as_calculation(calc_disc_factor),
-        as_calculation(calc_disc_cash),
-        as_calculation(calc_pv),
+        as_calculation(calcs["calc_v_mode"]),
+        as_calculation(calcs["calc_disc_factor"]),
+        as_calculation(calcs["calc_disc_cash"]),
+        as_calculation(calcs["calc_pv"]),
     )
-    return registry_base, registry_mode
+
+    registry_error = Registry(
+        as_calculation(calcs["calc_v"]),
+        as_calculation(calcs["calc_disc_factor"]),
+        as_calculation(calcs["calc_pv"]),
+    )
+
+    return {"base": registry_base, "mode": registry_mode, "error": registry_error}
 
 
-class TestFFFunction:
-    pass
+def test_model_graph(ddf, registry):
+    model_graph_base = ModelGraph(ddf._meta, registry["base"])
+    model_graph_mode = ModelGraph(ddf._meta, registry["mode"])
+
+    # test _get_settings
+    # assert model_graph_base.settings == {}
+    # assert model_graph_mode.settings == ""
+
+    # test_get_functions
+    # assert model_graph_base.functions ==
+    # assert model_graph_mode.functions ==
+
+    # test error
+    # pytest.raises(AssertionError, ModelGraph, ddf._meta, registry["registry_error"])
 
 
-# @pytest.mark.usefixtures("ddf", "registry")
-class TestModelGraph:
-    """"
-    """
+def test_model_template(ddf, registry):
+    model_temp_base = ModelTemplate(ddf._meta, registry["base"])
+    model_temp_runtime = ModelTemplate(
+        ddf._meta, registry["mode"], runtime_settings=["mode"]
+    )
+    model_temp_defined = ModelTemplate(ddf._meta, registry["mode"], mode="M")
 
-    def test_model_graph(self, ddf, registry):
-        registry_base, registry_mode = registry
-        model_graph_1 = ModelGraph(ddf._meta, registry_base)
-        model_graph_2 = ModelGraph(ddf._meta, registry_mode)
+    # test_runtime_settings
+    assert model_temp_base.runtime_settings is None
+    # assert model_temp_runtime.runtime_settings is None
+    assert model_temp_defined.runtime_settings is None
 
+    # test_runtime_checks
 
-class TestModelTemplate:
-    def test_runtime_settings(self):
-        pass
+    # test_defined_settings
 
-    def test_runtime_checks(self):
-        pass
+    # test_scenario
 
-    def test_defined_settings(self):
-        pass
+    # test_step
 
-    def test_scenario(self):
-        pass
+    # test_instructions
 
-    def test_step(self):
-        pass
+    # test_dask_functions
 
-    def test_instructions(self):
-        pass
-
-    def test_dask_functions(self):
-        pass
-
-    def test_dask_meta(self):
-        pass
+    # test_dask_meta
 
 
-class TestModel:
-    def test_model(self, df, ddf, registry, calcs):
-        calc_v, calc_v_mode, calc_disc_factor, calc_disc_cash, calc_pv = calcs
-        registry_base, registry_mode = registry
+def test_model(df, ddf, registry, calcs):
 
-        m1_t = ModelTemplate(frame_meta=ddf._meta, registry=registry_base)
-        m1_a = m1_t(frame=ddf)
-        m1_b = Model(frame=ddf, registry=registry_base)
+    m1_t = ModelTemplate(frame_meta=ddf._meta, registry=registry["base"])
+    m1_a = m1_t(frame=ddf)
+    m1_b = Model(frame=ddf, registry=registry["base"])
 
-        test1 = df.assign(
-            v=lambda x: calc_v(x["i"]),
-            disc_factor=lambda x: calc_disc_factor(x["v"]),
-            disc_cash=lambda x: calc_disc_cash(x["cash"], x["disc_factor"]),
-            pv=lambda x: calc_pv(x["disc_cash"]),
-        )
+    test1 = df.assign(
+        v=lambda x: calcs["calc_v"](x["i"]),
+        disc_factor=lambda x: calcs["calc_disc_factor"](x["v"]),
+        disc_cash=lambda x: calcs["calc_disc_cash"](x["cash"], x["disc_factor"]),
+        pv=lambda x: calcs["calc_pv"](x["disc_cash"]),
+    )
 
-        assert m1_a.compute().equals(test1)  # assert_frame_equal(m1_a.compute(), test1)
-        assert m1_b.compute().equals(test1)  # assert_frame_equal(m1_b.compute(), test1)
+    assert_frame_equal(m1_a.compute(), test1)
+    assert_frame_equal(m1_b.compute(), test1)
 
-        m2_t = ModelTemplate(
-            frame_meta=ddf._meta, registry=registry_mode, runtime_settings=["mode"]
-        )
-        m2_a = m2_t(frame=ddf, mode="A")
-        m2_b = Model(frame=ddf, registry=registry_mode, mode="A")
+    m2_t = ModelTemplate(
+        frame_meta=ddf._meta, registry=registry["mode"], runtime_settings=["mode"]
+    )
+    m2_a = m2_t(frame=ddf, mode="M")
+    m2_b = Model(frame=ddf, registry=registry["mode"], mode="M")
 
-        test2 = df.assign(
-            v=lambda x: calc_v_mode(x["i"], mode="A"),
-            disc_factor=lambda x: calc_disc_factor(x["v"]),
-            disc_cash=lambda x: calc_disc_cash(x["cash"], x["disc_factor"]),
-            pv=lambda x: calc_pv(x["disc_cash"]),
-        )
+    test2 = df.assign(
+        v=lambda x: calcs["calc_v_mode"](x["i"], mode="M"),
+        disc_factor=lambda x: calcs["calc_disc_factor"](x["v"]),
+        disc_cash=lambda x: calcs["calc_disc_cash"](x["cash"], x["disc_factor"]),
+        pv=lambda x: calcs["calc_pv"](x["disc_cash"]),
+    )
 
-        assert m2_a.compute().equals(test2)  # assert_frame_equal(m2_a.compute(), test2)
-        assert m2_b.compute().equals(test2)  # assert_frame_equal(m2_b.compute(), test2)
+    assert_frame_equal(m2_a.compute(), test2)
+    assert_frame_equal(m2_b.compute(), test2)
