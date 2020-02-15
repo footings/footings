db2f28074517daa5cec00d08d61c5dfaf1a8811b
Dustin Tindall <dustin.tindall@gmail.com>
2020-02-15 14:16:09 -0600

    brought dask.delayed

--- footings/model.py
+++ footings/model.py
@@ -3,7 +3,11 @@
 # from typing import List
 from inspect import getfullargspec
 
-from dask import threaded, multiprocessing 
+from dask.base import dont_optimize
+from dask.context import globalmethod
+from dask import threaded
+from dask.delayed import optimize, single_key, rebuild, delayed
+import dask.dataframe as dd
 from attr import attrs, attrib, make_class
 
 from .parameter import Parameter
@@ -27,9 +31,36 @@ class ModelParameterDoesNotExist(Exception):
 @attrs(slots=True)
 class _BaseModel:
     """BaseModel"""
-
+    _dask = attrib(init=False)
+    _key = attrib(init=False)
     _scenarios = {}
     _plan = []
+    
+    def __attrs_post_init__(self):
+        self._dask = _create_dask_graph
+
+    def __dask_graph__(self):
+        return self._dask
+
+    def __dask_keys__(self):
+        return [self._key]
+
+    def __dask_layers__(self):
+        return (self._key,)
+
+    def __dask_tokenize__(self):
+        return self._key
+
+    __dask_optimize__ = globalmethod(
+        optimize, key="dataframe_optimize", falsey=dont_optimize
+    )
+    __dask_scheduler__ = staticmethod(threaded.get)
+
+    def __dask_postcompute__(self):
+        return single_key, ()
+
+    def __dask_postpersist__(self):
+        return rebuild, (self._key, getattr(self, "_length", None))
 
     @classmethod
     def register_scenario(cls, name, **kwargs):
@@ -51,52 +82,6 @@ class _BaseModel:
             raise ModelScenarioDoesNotExist(f"The scenario [{name}] does not exist.")
         return cls(**kwargs, **cls._scenarios.get(name))
 
-    def _run_audit(self, **kwargs):
-        """Run audit function"""
-        dict_ = {}
-        for step in self._plan:
-            if isinstance(step, TblSchema):
-                dict_.update({step.name: kwargs.get(step.name)})
-            else:
-                args = {}
-                for k, v in step.args.items():
-                    if isinstance(v, GetPrior):
-                        args.update({k: v.get(dict_)})
-                    else:
-                        args.update({k: kwargs.get(k)})
-                dict_.update({step.name: step.function(**args)})
-        return dict_
-
-    def _run_client(self, client, **kwargs):
-        """Run client function"""
-        raise NotImplementedError()
-
-    def _run_default(self, **kwargs):
-        """Run default function"""
-        prior = {}
-        for step in self._plan:
-            if isinstance(step, TblSchema):
-                prior.update({step.name: kwargs.get(step.name)})
-            else:
-                args = {}
-                for k, v in step.args.items():
-                    if isinstance(v, GetPrior):
-                        args.update({k: v.get(prior)})
-                    else:
-                        args.update({k: kwargs.get(k)})
-                prior.update({step.name: step.function(**args)})
-        return prior
-
-    def run(self, audit=False, client=None):
-        """Run model"""
-        args = getfullargspec(self).args
-        if audit:
-            return self._run_audit(**{arg: getattr(self, arg) for arg in args})
-        if client is True:
-            return self._run_client(
-                **{arg: getattr(self, arg) for arg in args}, client=client
-            )
-        return self._run_default(**{arg: getattr(self, arg) for arg in args})
 
 
 def _create_attributes(plan):
