54c7d56bdaf0d423bba71e0b709aedb4299fa332
Dustin Tindall <dustin.tindall@gmail.com>
2020-03-05 09:45:22 -0600

    Refactored to dispatch on keys.

--- footings/utils.py
+++ footings/utils.py
@@ -1,9 +1,13 @@
 """utils.py"""
 
+
+from itertools import product, starmap
 from functools import partial
+from collections import namedtuple
+from collections.abc import Iterable
 
 from attr import attrs, attrib
-from attr.validators import optional, is_callable, instance_of
+from attr.validators import optional, is_callable, instance_of, deep_iterable
 
 
 class PriorStepError(Exception):
@@ -42,12 +46,17 @@ class DispatcherKeyError(Exception):
     """Key does not exist within dispatch function registry."""
 
 
+class DispatcherRegisterTypeError(Exception):
+    """The types passed to each keyword in register must be a str or an iterable."""
+
+
+class DispatcherRegisterParameterError(Exception):
+    """The parameter passed to register does not much the parameters of the instance."""
+
+
 def _update_registry(registry, keys, function):
-    if isinstance(keys, str):
-        registry.update({keys: function})
-    else:
-        for key in keys:
-            registry.update({key: function})
+    for key in keys:
+        registry.update({key: function})
     return registry
 
 
@@ -56,21 +65,43 @@ class Dispatcher:
     """Dispatch function"""
 
     name = attrib(validator=instance_of(str))
+    parameters = attrib(validator=deep_iterable(instance_of(str), instance_of(tuple)))
     default = attrib(default=None, validator=optional(is_callable()))
     registry = attrib(init=False, repr=False, factory=dict)
 
-    def register(self, keys, function=None):
-        """Register a key and function to dispatch on."""
+    def register(self, function=None, **kwargs):
+        """Register a key and function t o dispatch on."""
+        items = []
+        for param in self.parameters:
+            value = kwargs.get(param, None)
+            if value is None:
+                raise DispatcherRegisterParameterError(
+                    f"The parameter [{param}] is not a parameter of the instance."
+                )
+            if isinstance(value, str):
+                items.append([value])
+            elif isinstance(value, Iterable):
+                items.append(value)
+            else:
+                raise DispatcherRegisterTypeError(
+                    f"The value for [{param}] is not a str or an iterable."
+                )
+
+        keys = list(product(*items))
         if function is None:
             return partial(_update_registry, self.registry, keys)
         return _update_registry(self.registry, keys, function)
 
-    def __call__(self, key, *args, **kwargs):
+    def __call__(self, **kwargs):
+        key = []
+        for param in self.parameters:
+            key.append(kwargs.get(param))
+            kwargs.pop(param)
+        key = tuple(key)
         func = self.registry.get(key, None)
         if func is None:
             if self.default is None:
-                msg = f"The key [{key}] does not exist within registry and no default \
-                as fallback."
+                msg = f"The key {key} does not exist within registry and no default."
                 raise DispatcherKeyError(msg)
-            return self.default(*args, **kwargs)
-        return func(*args, **kwargs)
+            return self.default(**kwargs)
+        return func(**kwargs)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,78 +4,119 @@
 
 import pytest
 
-from footings.utils import Dispatcher, DispatcherKeyError
+from footings.utils import (
+    Dispatcher,
+    DispatcherKeyError,
+    DispatcherRegisterParameterError,
+    DispatcherRegisterTypeError,
+)
 
 
-def test_dispatch_one_key_no_default():
-    test = Dispatcher(name="test")
-
-    @test.register("x")
-    def _():
-        return "x"
-
-    @test.register("y")
-    def _():
-        return "y"
-
-    assert test("x") == "x"
-    assert test("y") == "y"
-    pytest.raises(DispatcherKeyError, test, "z")
-
-
-def test_dispatch_many_keys_no_default():
-    test = Dispatcher(name="test")
-
-    @test.register(["x1", "x2", "x3"])
-    def _():
-        return "x"
-
-    @test.register("y")
-    def _():
-        return "y"
-
-    assert test("x1") == "x"
-    assert test("x2") == "x"
-    assert test("x3") == "x"
-    assert test("y") == "y"
-    pytest.raises(DispatcherKeyError, test, "z")
-
-
-def test_dispatch_one_key_with_default():
+def test_dispatch_default():
     def _default():
         return "default"
 
-    test = Dispatcher(name="test", default=_default)
+    test = Dispatcher(name="test", parameters=("key",), default=_default)
 
-    @test.register("x1")
+    @test.register(key="x")
     def _():
         return "x"
 
-    @test.register("y")
+    @test.register(key="y")
     def _():
         return "y"
 
-    assert test("x1") == "x"
-    assert test("y") == "y"
-    assert test("z") == "default"
+    assert test(key="x") == "x"
+    assert test(key="y") == "y"
+    assert test(key="z") == "default"
 
 
-def test_dispatch_many_keys_with_default():
-    def _default():
-        return "default"
+def test_dispatch_one_parameter_single_value():
+    test = Dispatcher(name="test", parameters=("key",))
 
-    test = Dispatcher(name="test", default=_default)
-
-    @test.register(["x1", "x2", "x3"])
+    @test.register(key="x")
     def _():
         return "x"
 
-    @test.register("y")
+    @test.register(key="y")
     def _():
         return "y"
 
-    assert test("x1") == "x"
-    assert test("x2") == "x"
-    assert test("x3") == "x"
-    assert test("y") == "y"
-    assert test("z") == "default"
+    assert test(key="x") == "x"
+    assert test(key="y") == "y"
+    pytest.raises(DispatcherKeyError, test, key="z")
+
+
+def test_dispatch_one_parameter_multiple_values():
+    test = Dispatcher(name="test", parameters=("key",))
+
+    @test.register(key=["x1", "x2", "x3"])
+    def _():
+        return "x"
+
+    @test.register(key="y")
+    def _():
+        return "y"
+
+    assert test(key="x1") == "x"
+    assert test(key="x2") == "x"
+    assert test(key="x3") == "x"
+    assert test(key="y") == "y"
+    pytest.raises(DispatcherKeyError, test, key="z")
+
+
+def test_dispatch_multiple_parameters_single_value():
+    test = Dispatcher(name="test", parameters=("key1", "key2"))
+
+    @test.register(key1="x1", key2="x2")
+    def _():
+        return "x"
+
+    @test.register(key1="y1", key2="y2")
+    def _():
+        return "y"
+
+    assert test(key1="x1", key2="x2") == "x"
+    assert test(key1="y1", key2="y2") == "y"
+    pytest.raises(DispatcherKeyError, test, key1="x", key2="z")
+
+
+def test_dispatch_many_parameter_multiple_values():
+    test = Dispatcher(name="test", parameters=("key1", "key2", "key3"))
+
+    @test.register(key1=["x1", "x2", "x3"], key2=["xa", "xb"], key3="xz")
+    def _():
+        return "x"
+
+    @test.register(key1=["y1", "y2"], key2=["ya"], key3="y")
+    def _():
+        return "y"
+
+    assert test(key1="x1", key2="xa", key3="xz") == "x"
+    assert test(key1="x2", key2="xa", key3="xz") == "x"
+    assert test(key1="x3", key2="xa", key3="xz") == "x"
+    assert test(key1="x1", key2="xb", key3="xz") == "x"
+    assert test(key1="x2", key2="xb", key3="xz") == "x"
+    assert test(key1="x3", key2="xb", key3="xz") == "x"
+    assert test(key1="y1", key2="ya", key3="y") == "y"
+    assert test(key1="y2", key2="ya", key3="y") == "y"
+    pytest.raises(DispatcherKeyError, test, key1="x1", key2="xa", key3="z")
+
+
+def test_dispatch_raise_errors():
+    test = Dispatcher(name="test", parameters=("key",))
+
+    with pytest.raises(DispatcherRegisterParameterError):
+
+        @test.register(wrong_key="x")
+        def _():
+            return "x"
+
+    class Test:
+        pass
+
+    with pytest.raises(DispatcherRegisterTypeError):
+
+        @test.register(key=Test())
+        def _():
+            return "y"
