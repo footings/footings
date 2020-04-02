2b4ce27fc5ee5c4c0c7ef211e48a15b90dc46b79
Dustin Tindall <dustin.tindall@gmail.com>
2020-04-02 06:36:11 -0500

    Updates to __init__.py.

--- footings/__init__.py
+++ footings/__init__.py
@@ -1,12 +1,6 @@
 """__init__.py"""
 
 from footings.argument import Argument
-from footings.footing import Footing, footing_from_list
-from footings.schema import (
-    ColSchema,
-    TblSchema,
-    table_schema_from_json,
-    table_schema_from_yaml,
-)
-from footings.utils import Dispatcher
+from footings.footing import Footing, create_footing_from_list
+from footings.utils import DispatchFunction, LoadedFunction
 from footings.model import build_model
