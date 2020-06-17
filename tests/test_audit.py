8639c6b7361d290e5b28d608a6577fae4e541e89
Dustin Tindall <dustin.tindall@gmail.com>
2020-06-17 04:52:24 -0500

    Removed [Content_Types].xml from xml check of excel files.

--- tests/test_audit.py
+++ tests/test_audit.py
@@ -23,11 +23,15 @@ def test_audit_xlsx(tmp_path):
     integer_model = create_model("IntegerModel", steps=STEPS_USING_INTEGERS)
     loaded_integer_model = integer_model(a=1, b=1, c=1)
     loaded_integer_model.audit(output_type="xlsx", file=test_integer_out)
-    assert compare_xlsx_files(test_integer_out, expected_integer_out)
+    assert compare_xlsx_files(
+        test_integer_out, expected_integer_out, ["[Content_Types].xml"], {}
+    )
 
     test_pandas_out = os.path.join(tmp_path, "test-pandas.xlsx")
     expected_pandas_out = os.path.join("tests", "data", "expected-pandas.xlsx")
     pandas_model = create_model("PandasModel", steps=STEPS_USING_PANDAS)
     loaded_pandas_model = pandas_model(n=5, add=1, subtract=1)
     loaded_pandas_model.audit(output_type="xlsx", file=test_pandas_out)
-    assert compare_xlsx_files(test_pandas_out, expected_pandas_out)
+    assert compare_xlsx_files(
+        test_pandas_out, expected_pandas_out, ["[Content_Types].xml"], {}
+    )
