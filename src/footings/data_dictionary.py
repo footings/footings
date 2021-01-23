325cb9991c69f7a56c60c55999f9a9ea55776559
Dustin Tindall <dustin.tindall@gmail.com>
2021-01-23 12:21:53 -0600

    Added data dictionary object.

---
+++ src/footings/data_dictionary.py
@@ -0,0 +1,360 @@
+from enum import Enum
+from functools import partial
+from typing import List, Mapping, Optional, Union
+
+from attr import attrs, attrib, make_class
+from attr.validators import instance_of, optional
+import numpy as np
+import pandas as pd
+
+from .attributes import def_parameter
+from .exceptions import (
+    DataDictionaryValidatorsConversionError,
+    DataDictionaryPandasDtypeConversionError,
+    DataDictionaryValidateError,
+)
+
+
+__all__ = ["PandasDtype", "data_dictionary", "def_column"]
+
+
+class PandasDtype(Enum):
+    """Enum container of all valid pandas data types.
+
+    This was copied from the `pandera` project and modified.
+    """
+
+    Bool = "bool"
+    DateTime = "datetime64[ns]"
+    Timedelta = "timedelta64[ns]"
+    Category = "category"
+    Float = "float64"
+    Float16 = "float16"
+    Float32 = "float32"
+    Float64 = "float64"
+    Int = "int64"
+    Int8 = "Int8"
+    Int16 = "Int16"
+    Int32 = "Int32"
+    Int64 = "Int64"
+    UInt8 = "UInt8"
+    UInt16 = "UInt16"
+    UInt32 = "UInt32"
+    UInt64 = "UInt64"
+    Object = "object"
+    Complex = "complex128"
+    Complex64 = "complex64"
+    Complex128 = "complex128"
+    Complex256 = "complex256"
+    String = "string"
+
+
+PANDAS_DTYPE_STR_ALIAS = {
+    "bool": PandasDtype.Bool,
+    "datetime64[ns]": PandasDtype.DateTime,
+    "timedelta64[ns]": PandasDtype.Timedelta,
+    "category": PandasDtype.Category,
+    "float": PandasDtype.Float,
+    "float16": PandasDtype.Float16,
+    "float32": PandasDtype.Float32,
+    "float64": PandasDtype.Float64,
+    "int": PandasDtype.Int,
+    "int8": PandasDtype.Int8,
+    "int16": PandasDtype.Int16,
+    "int32": PandasDtype.Int32,
+    "int64": PandasDtype.Int64,
+    "uint8": PandasDtype.UInt8,
+    "uint16": PandasDtype.UInt16,
+    "uint32": PandasDtype.UInt32,
+    "uint64": PandasDtype.UInt64,
+    "object": PandasDtype.Object,
+    "complex": PandasDtype.Complex,
+    "complex64": PandasDtype.Complex64,
+    "complex128": PandasDtype.Complex128,
+    "complex256": PandasDtype.Complex256,
+    "str": PandasDtype.String,
+    "string": PandasDtype.String,
+}
+
+
+PANDAS_DTYPE_CONVERSION = {
+    PandasDtype.Bool: bool,
+    PandasDtype.DateTime: pd.Timestamp,
+    PandasDtype.Timedelta: pd.Timedelta,
+    PandasDtype.Category: str,
+    PandasDtype.Float: float,
+    PandasDtype.Float16: float,
+    PandasDtype.Float32: float,
+    PandasDtype.Float64: float,
+    PandasDtype.Int: int,
+    PandasDtype.Int8: np.int8,
+    PandasDtype.Int16: np.int16,
+    PandasDtype.Int32: np.int32,
+    PandasDtype.Int64: np.int64,
+    PandasDtype.UInt8: np.uint8,
+    PandasDtype.UInt16: np.uint16,
+    PandasDtype.UInt32: np.uint32,
+    PandasDtype.UInt64: np.uint64,
+    PandasDtype.Object: str,
+    PandasDtype.Complex: complex,
+    PandasDtype.Complex64: complex,
+    PandasDtype.Complex128: complex,
+    PandasDtype.Complex256: complex,
+    PandasDtype.String: str,
+}
+
+
+def converter_pandas_dtype(x: Union[str, PandasDtype]):
+    if x is None:
+        return None
+    if isinstance(x, str):
+        x_upd = getattr(PandasDtype, x, None)  # try to get attribute
+        if x_upd is None:  # if attribute fails lookup string alias
+            x_upd = PANDAS_DTYPE_STR_ALIAS.get(x, None)
+        if x_upd is None:  # if both getting attribute and string alias fail raise error.
+            msg = f"The passed string [{x}] is not a built-in PandasDtype or a valid string alias."
+            raise DataDictionaryPandasDtypeConversionError(msg)
+        return x_upd
+    elif isinstance(x, PandasDtype) is False:
+        msg = "The passed object is not a built-in PandasDtype or a valid string alias."
+        raise DataDictionaryPandasDtypeConversionError(msg)
+    return x
+
+
+def converter_validators(value: Union[str, callable]):
+    def inner(x):
+        if isinstance(x, str):
+            msg = "The feature to look up a validator by string name is not implemented yet."
+            raise NotImplementedError(msg)
+        elif not callable(x):
+            msg = "The passed object needs to be a callable object."
+            raise DataDictionaryValidatorsConversionError(msg)
+        return x
+
+    if isinstance(value, list):
+        return [inner(x) for x in value]
+    return [inner(value)]
+
+
+@attrs(frozen=True, slots=True)
+class Column:
+    """Container for column kwargs to pass into `attrs.attrib`.
+
+    :param str name: The name of the column.
+    :param Optional[Union[str, PandasDtype]] dtype: The column data type.
+    :param Optional[str] description: The column description.
+    :param Optional[Union[List[callable], callable]] validator: Column validators.
+    :param Optional[Mapping] metadata: Optional metadata to attach to column.
+    """
+
+    name = attrib(type=str, kw_only=True, validator=instance_of(str))
+    dtype = attrib(
+        type=Optional[Union[str, PandasDtype]],
+        default=None,
+        converter=converter_pandas_dtype,
+        kw_only=True,
+    )
+    description = attrib(
+        type=Optional[str],
+        default=None,
+        kw_only=True,
+        validator=optional(instance_of(str)),
+    )
+    validator = attrib(
+        type=Optional[Union[List[callable], callable]],
+        factory=list,
+        kw_only=True,
+        converter=converter_validators,
+    )
+    metadata = attrib(
+        type=Mapping, factory=dict, kw_only=True, validator=instance_of(Mapping)
+    )
+
+
+def def_column(
+    dtype: Optional[Union[str, PandasDtype]] = None,
+    description: Optional[str] = None,
+    validator: Optional[Union[List[callable], callable]] = None,
+    metadata: Optional[Mapping] = None,
+) -> partial:
+    """Define a column with a data dictionary.
+
+    :param Optional[Union[str, PandasDtype]] dtype: The column data type.
+    :param Optional[str] description: The column description.
+    :param Optional[Union[List[callable], callable]] validator: Column validators.
+    :param Optional[Mapping] metadata: Optional metadata to attach to column.
+
+    :return: A partially completed Column object (missing name which is added
+        the data dictioanry is created).
+    :rtype: partial
+    """
+
+    if validator is None:
+        validator = []
+    if metadata is None:
+        metadata = {}
+    return partial(
+        Column,
+        dtype=dtype,
+        description=description,
+        validator=validator,
+        metadata=metadata,
+    )
+
+
+@attrs(frozen=True, slots=True, repr=False)
+class DataDictionary:
+    """The parent class of a container that provides information on a tabular data structure."""
+
+    __columns__ = attrib(init=False, repr=False)
+
+    def columns(self):
+        return [getattr(self, col) for col in self.__columns__]
+
+    def __repr__(self):
+        name = self.__class__.__qualname__
+        cols = "\n\t".join([str(getattr(self, col)) for col in self.__columns__])
+        return f"{name}(DataDictionary)\n\t{cols}\n"
+
+    def _cols_valid(self, dataframe: pd.DataFrame):
+        """Test match of columns in datadictionary vs dataframe."""
+        results, msgs = [], []
+        col_dict = {col.name: col for col in self.columns()}
+        cols = list(dict.fromkeys(list(col_dict) + list(dataframe.columns)))
+        for col in cols:
+            dd_col = col_dict.get(col, None)
+            df_col = dataframe.get(col, None)
+            if dd_col is None:
+                results.append(False)
+                msg = f"The column [{col}] is missing from the DataDictionary but is in the dataframe."
+                msgs.append(msg)
+            if df_col is None:
+                results.append(False)
+                msg = f"The column [{col}] is missing from the dataframe but is in the DataDictionary."
+                msgs.append(msg)
+            if dd_col is not None and df_col is not None:
+                results.append(True)
+        return results, msgs
+
+    def _types_valid(self, dataframe: pd.DataFrame):
+        """Test column types in dataframe."""
+        results, msgs = [], []
+        for dd_col in self.columns():
+            df_col = dataframe.get(dd_col.name, None)
+            test_eq = dd_col.dtype.value == df_col.dtype.name
+            if test_eq is False:
+                msg = f"The column [{dd_col.name}] in the DataDictionary has type [{dd_col.dtype.value}] "
+                msg += f"which is different from type in the dataframe passed [{df_col.dtype.name}]."
+                msgs.append(msg)
+            results.append(test_eq)
+
+        return results, msgs
+
+    def _validators_valid(self, dataframe: pd.DataFrame):
+        """Test validators for columns in dataframe."""
+        results, msgs = [], []
+        for col in self.columns():
+            df_col = dataframe.get(col.name)
+            if len(col.validator) > 0:
+                for validator in col.validator:
+                    try:
+                        validator(inst=self, attr=col, value=df_col)
+                        results.append(True)
+                    except:
+                        results.append(False)
+                        msgs.append(
+                            f"The column [{col.name}] failed {str(validator)[1:-1]}."
+                        )
+
+        return results, msgs
+
+    def validate(
+        self, dataframe: pd.DataFrame, types: bool = True, validators: bool = True
+    ):
+        """Validate passed dataframe types and/or validators against DataDictionary.
+
+        :param pd.DataFrame dataframe: The dataframe to validate.
+        :param bool types: Test DataDictionary types against dataframe. Default is True.
+        :param bool validators: Test DataDictionary validators against dataframe. Default is True.
+
+        :return: True if test pass.
+        :rtype: bool
+
+        :raises DataDictionaryValidateError: If any of the following rules are broken -
+            - All columns present in the data dictionary are in the dataframe and vice versa.
+            - If types is True, test the data types of the dataframe match the data dictionary.
+            - If validators is True, test that any attached validator pass for a column.
+        """
+        rets, msgs = self._cols_valid(dataframe)
+
+        def _update_ret_msg(fn, dataframe):
+            ret, msg = fn(dataframe)
+            rets.extend(ret)
+            msgs.extend(msg)
+
+        if types is True:
+            _update_ret_msg(self._types_valid, dataframe)
+        if validators is True:
+            _update_ret_msg(self._validators_valid, dataframe)
+
+        if not all(rets):
+            msg = "\n".join(msgs)
+            raise DataDictionaryValidateError("\n" + msg)
+        return True
+
+    def def_parameter(
+        self,
+        column: str,
+        add_dtype_validator: bool = False,
+        dtype_mapping: Mapping = PANDAS_DTYPE_CONVERSION,
+    ):
+        """Define a parameter for a model from a data dictioanry column.
+
+        :param str column: The column name.
+        :param bool add_type_validator: If True, an instance_of validator is added to the parameter.
+        :param Mapping dtype_mapping: A conversion mapping of PandasDtypes to different types.
+        """
+        col = getattr(self, column, None)
+        if col is None:
+            msg = f"The column [{column}] does not belong to the data dictionary."
+            raise AttributeError(msg)
+        dtype = dtype_mapping.get(col.dtype)
+        validator = col.validator.copy()
+        if add_dtype_validator is True:
+            validator.append(instance_of(dtype))
+        return def_parameter(
+            dtype=dtype, description=col.description, validator=validator,
+        )
+
+    def def_sensitivity(self, column):
+        raise NotImplementedError("This feature is not implemented yet.")
+
+    def def_meta(self, column):
+        raise NotImplementedError("This feature is not implemented yet.")
+
+
+def make_col_attrib(cls, col):
+    column = getattr(cls, col)(name=col)
+    return attrib(default=column, init=False)
+
+
+def data_dictionary(cls: type = None):
+    """A decorator that creates a custom data dictionary object."""
+    if cls is None:
+        return partial(data_dictionary)
+
+    def make_data_dictionary(cls):
+        exclude = [x for x in DataDictionary.__dict__.keys() if x[0] != "_"]
+        cols = [x for x in cls.__dict__.keys() if x[0] != "_" and x not in exclude]
+        attrs = {col: make_col_attrib(cls, col) for col in cols}
+        attrs.update({"__columns__": attrib(default=cols, init=False, repr=False)})
+        return make_class(
+            cls.__name__,
+            attrs,
+            bases=(DataDictionary,),
+            slots=True,
+            frozen=True,
+            repr=False,
+        )
+
+    return make_data_dictionary(cls)()  # note an instance is returned
---
+++ tests/test_data_dictionary.py
@@ -0,0 +1,313 @@
+from functools import partial
+import re
+
+from attr import asdict
+import pandas as pd
+import pytest
+
+from footings import model
+from footings.data_dictionary import (
+    PandasDtype,
+    Column,
+    def_column,
+    DataDictionary,
+    data_dictionary,
+)
+from footings.validators import equal_to
+from footings.exceptions import (
+    DataDictionaryValidatorsConversionError,
+    DataDictionaryPandasDtypeConversionError,
+    DataDictionaryValidateError,
+)
+
+
+def clean_str(x):
+    return re.sub(r"\s+", "", x, flags=re.UNICODE)
+
+
+def create_df():
+
+    df = pd.DataFrame()
+    df["bool"] = pd.Series([True, False], dtype="bool")
+    df["datetime64[ns]"] = pd.Series([0, 1], dtype="datetime64[ns]")
+    df["timedelta64[ns]"] = pd.Series([0, 1], dtype="timedelta64[ns]")
+    df["category"] = pd.Series(["a", "b"], dtype="category")
+    df["float"] = pd.Series([1.1, 1.2], dtype="float")
+    df["float16"] = pd.Series([1.1, 1.2], dtype="float16")
+    df["float32"] = pd.Series([1.1, 1.2], dtype="float32")
+    df["float64"] = pd.Series([1.1, 1.2], dtype="float64")
+    df["int"] = pd.Series([1, 2], dtype="int")
+    df["int8"] = pd.Series([1, 2], dtype="Int8")
+    df["int16"] = pd.Series([1, 2], dtype="Int16")
+    df["int32"] = pd.Series([1, 2], dtype="Int32")
+    df["int64"] = pd.Series([1, 2], dtype="Int64")
+    df["uint8"] = pd.Series([1, 2], dtype="UInt8")
+    df["uint16"] = pd.Series([1, 2], dtype="UInt16")
+    df["uint32"] = pd.Series([1, 2], dtype="UInt32")
+    df["uint64"] = pd.Series([1, 2], dtype="UInt64")
+    df["object"] = pd.Series(["a", "b"], dtype="object")
+    df["complex"] = pd.Series([complex(1), complex(2)], dtype="complex")
+    df["complex64"] = pd.Series([complex(1), complex(2)], dtype="complex64")
+    df["complex128"] = pd.Series([complex(1), complex(2)], dtype="complex128")
+    df["complex256"] = pd.Series([complex(1), complex(2)], dtype="complex256")
+    df["string"] = pd.Series(["str1", "str2"], dtype="string")
+
+    return df
+
+
+class TestColumn:
+    """Test Column class"""
+
+    def test_instance(self):
+        assert isinstance(Column(name="COL"), Column)
+
+    def test_kw_only(self):
+        with pytest.raises(TypeError):
+            Column("COL")
+
+    def test_attributes(self):
+        col1 = Column(name="COL")
+        assert col1.dtype is None
+        assert col1.description is None
+        assert col1.validator == []
+        assert col1.metadata == {}
+
+        col2 = Column(name="COL", dtype="bool", description="COL description...")
+        assert col2.dtype is PandasDtype.Bool
+        assert col2.description == "COL description..."
+        assert col2.validator == []
+        assert col2.metadata == {}
+
+    def test_dtype(self):
+        # test converter_pandas_dtype
+        assert Column(name="COL").dtype is None
+        assert Column(name="COL", dtype="bool").dtype is PandasDtype.Bool
+        assert Column(name="COL", dtype="Bool").dtype is PandasDtype.Bool
+        assert Column(name="COL", dtype=PandasDtype.Bool).dtype is PandasDtype.Bool
+        with pytest.raises(DataDictionaryPandasDtypeConversionError):
+            Column(name="COL", dtype="x")
+
+    def test_validator(self):
+        # test converter_validators
+        def val1():
+            pass
+
+        def val2():
+            pass
+
+        assert Column(name="COL", validator=val1).validator == [val1]
+        assert Column(name="COL", validator=[val1]).validator == [val1]
+        assert Column(name="COL", validator=[val1, val2]).validator == [val1, val2]
+        with pytest.raises(NotImplementedError):
+            # lookup validator not implemented yet
+            Column(name="COL", validator="get_validator")
+        with pytest.raises(DataDictionaryValidatorsConversionError):
+            # validator needs to be callable
+            Column(name="COL", validator=1)
+
+    def test_metadata(self):
+        meta = {"extra": "info"}
+        assert Column(name="COL", metadata=meta).metadata == meta
+        with pytest.raises(TypeError):
+            Column(name="COL", metadata="x")  # metadata needs to be mapping
+
+
+def test_def_column():
+    col = def_column(dtype="bool")
+    assert isinstance(col, partial)
+    assert col.keywords == {
+        "dtype": "bool",
+        "description": None,
+        "validator": [],
+        "metadata": {},
+    }
+
+
+@pytest.fixture(scope="session", name="DD")
+def dd_success():
+    @data_dictionary
+    class TestDD:
+        COL1 = def_column(
+            dtype="int", description="This is column 1.", validator=equal_to(1)
+        )
+        COL2 = def_column(dtype="str", description="This is column 2.")
+
+    return TestDD
+
+
+@pytest.fixture(scope="session")
+def df_correct():
+    df = pd.DataFrame(
+        {
+            "COL1": pd.Series([1, 1, 1], dtype=int),
+            "COL2": pd.Series(["a", "b", "c"], dtype="string"),
+        }
+    )
+    return df
+
+
+@pytest.fixture(scope="session")
+def df_missing_column():
+    df = pd.DataFrame({"COL1": pd.Series([1, 1, 1], dtype=int),})
+    return df
+
+
+@pytest.fixture(scope="session")
+def df_extra_column():
+    df = pd.DataFrame(
+        {
+            "COL1": pd.Series([1, 1, 1], dtype=int),
+            "COL2": pd.Series(["a", "b", "c"], dtype="string"),
+            "COL3": pd.Series([1, 1, 1], dtype=int),
+        }
+    )
+    return df
+
+
+@pytest.fixture(scope="session")
+def df_wrong_type():
+    df = pd.DataFrame(
+        {
+            "COL1": pd.Series([1, 1, 1], dtype=int),
+            "COL2": pd.Series([1, 1, 1], dtype=int),
+        }
+    )
+    return df
+
+
+@pytest.fixture(scope="session")
+def df_fail_validator():
+    df = pd.DataFrame(
+        {
+            "COL1": pd.Series([1, 1, 2], dtype=int),
+            "COL2": pd.Series(["a", "b", "c"], dtype="string"),
+        }
+    )
+    return df
+
+
+@pytest.fixture(scope="session")
+def df_wrong_multiple():
+    df = pd.DataFrame(
+        {
+            "COL1": pd.Series([1, 1, 2], dtype=int),
+            "COL2": pd.Series([1, 1, 1], dtype=int),
+            "COL3": pd.Series([1, 1, 1], dtype=int),
+        }
+    )
+    return df
+
+
+def compare_attributes(result, expected):
+    result = asdict(result)
+    del result["counter"]
+    expected = asdict(expected)
+    del expected["counter"]
+    return result == expected
+
+
+class TestDataDictionary:
+    """Test for data dictionary"""
+
+    def test_instance(self, DD):
+        assert isinstance(DD, DD.__class__)
+        assert isinstance(DD, DataDictionary)
+
+    def test_subclass(self, DD):
+        assert issubclass(DD.__class__, DataDictionary)
+
+    def test_cols_valid(self, DD, df_correct, df_extra_column, df_missing_column):
+        assert DD._cols_valid(df_correct) == ([True, True], [],)
+        assert DD._cols_valid(df_extra_column) == (
+            [True, True, False],
+            [
+                "The column [COL3] is missing from the DataDictionary but is in the dataframe."
+            ],
+        )
+        assert DD._cols_valid(df_missing_column) == (
+            [True, False],
+            [
+                "The column [COL2] is missing from the dataframe but is in the DataDictionary."
+            ],
+        )
+
+    def test_types_valid(self, DD, df_correct, df_wrong_type):
+
+        assert DD._types_valid(df_correct) == ([True, True], [],)
+        assert DD._types_valid(df_wrong_type) == (
+            [True, False],
+            [
+                "".join(
+                    [
+                        "The column [COL2] in the DataDictionary has type [string] which ",
+                        "is different from type in the dataframe passed [int64].",
+                    ]
+                )
+            ],
+        )
+
+    def test_validators_valid(self, DD, df_correct, df_fail_validator):
+        assert DD._validators_valid(df_correct) == ([True], [],)
+        assert DD._validators_valid(df_fail_validator) == (
+            [False],
+            ["The column [COL1] failed equal_to validator using 1 as the test."],
+        )
+
+    def test_validate(self, DD, df_correct, df_wrong_multiple):
+        assert DD.validate(df_correct) is True
+        with pytest.raises(DataDictionaryValidateError) as e:
+            DD.validate(df_wrong_multiple)
+        res_str = f"{clean_str(str(e.value))}"
+        errors = "\n".join(
+            [
+                "The column [COL3] is missing from the DataDictionary but is in the dataframe.",
+                "".join(
+                    [
+                        "The column [COL2] in the DataDictionary has type [string]",
+                        " which is different from type in the dataframe passed [int64].",
+                    ]
+                ),
+                "The column [COL1] failed equal_to validator using 1 as the test.",
+            ]
+        )
+        exp_str = clean_str(errors)
+        assert res_str == exp_str
+        pytest.raises(DataDictionaryValidateError, DD.validate, df_wrong_multiple)
+
+    def test_def_parameter(self, DD):
+        @model
+        class TestDD:
+            col1 = DD.def_parameter("COL1")
+            col1_mapping = DD.def_parameter("COL1", dtype_mapping={PandasDtype.Int: str})
+            col2 = DD.def_parameter("COL2")
+            col2_validator = DD.def_parameter("COL2", add_dtype_validator=True)
+
+        assert TestDD.__attrs_attrs__.col1.type == int
+        assert TestDD.__attrs_attrs__.col1_mapping.type == str
+        assert TestDD.__attrs_attrs__.col2.type == str
+        assert TestDD.__attrs_attrs__.col2_validator.type == str
+        assert isinstance(
+            TestDD(col1=1, col1_mapping=1, col2=1, col2_validator="x"), TestDD
+        )
+        with pytest.raises(TypeError):  # fails due col2_validator should be str
+            TestDD(col1=1, col1_mapping=1, col2=1, col2_validator=1)
+        with pytest.raises(ValueError):  # fails because col1 is 2
+            TestDD(col1=2, col1_mapping=1, col2="x", col2_validator="x")
+        with pytest.raises(ValueError):  # fails because co1_mapping is 2
+            TestDD(col1=1, col1_mapping=2, col2="x", col2_validator="x")
+
+    def test_def_sensitivity(self, DD):
+        pytest.raises(NotImplementedError, DD.def_sensitivity, "COL1")
+
+    def test_def_meta(self, DD):
+        pytest.raises(NotImplementedError, DD.def_meta, "COL1")
+
+    def test_repr(self, DD):
+
+        repr_str = """TestDD(DataDictionary)
+        Column(name='COL1', dtype=<PandasDtype.Int: 'int64'>, description='This is column 1.', validator=
+        [<equal_tovalidatorusing1asthetest>], metadata={})
+        Column(name='COL2', dtype=<PandasDtype.String: 'string'>, description='This is column 2.', validator=[],
+        metadata={})
+        """
+        assert clean_str(repr(DD)) == clean_str(repr_str)
