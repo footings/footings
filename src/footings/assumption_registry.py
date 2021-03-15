b18d57be43fed3b460a4a9de03bc98918782840f
Dustin Tindall <dustin.tindall@gmail.com>
2021-03-15 07:09:35 -0500

    Additional update to assumption registry.

--- src/footings/assumption_registry.py
+++ src/footings/assumption_registry.py
@@ -1,63 +1,185 @@
 from functools import partial
-from typing import Tuple, Union
+from inspect import signature
+from typing import Dict, Union
 
 from attr import attrib, attrs, make_class
-from attr.validators import instance_of, optional
+from attr.validators import instance_of, is_callable, optional
+
+
+def assumption_doc_generator(self):
+    def _get(x):
+        return f"[get('{x}', *args, **kwargs)]"
+
+    if self.name != self.assumption.__name__:
+        ret = f"**{self.name} {_get(self.assumption.__name__)} :**\n"
+    else:
+        ret = f"**{self.name} :**\n"
+    if self.description is not None and self.description != "":
+        ret += "" + self.description + "\n\n"
+    elif self.assumption.__doc__ is not None:
+        ret += "" + self.assumption.__doc__ + "\n\n"
+    else:
+        ret += "\n"
+    # if len(self.uses) > 0:
+    #     ret += "Uses - \n"
+    #     ret += "\n".join(self.uses)
+    # if len(self.metadata) > 0:
+    #     ret += "Metadata - \n"
+    #     ret += "\n".join([f"{k}: {v}" for k, v in self.metadata.items()])
+    return ret
+
+
+@attrs(frozen=True)  # slots=True is excluded so we can update the signature on init
+class Assumption:
+    name = attrib(type=str, validator=instance_of(str))
+    description = attrib(type=str, validator=instance_of(str))
+    uses = attrib(type=tuple, validator=instance_of(tuple))
+    assumption = attrib(type=callable, validator=is_callable())
+    metadata = attrib(type=dict, validator=instance_of(dict))
+    doc_generator = attrib(type=callable, validator=is_callable())
+
+    def __attrs_post_init__(self):
+        object.__setattr__(self, "__signature__", signature(self.assumption))
+        object.__setattr__(self, "__doc__", self.doc_generator(self))
+
+    @classmethod
+    def create(
+        cls,
+        *,
+        assumption: callable,
+        name: Union[str, None] = None,
+        description: Union[str, None] = None,
+        uses: Union[tuple, None] = None,
+        cache: Union[tuple, None] = None,
+        metadata: Union[dict, None] = None,
+        doc_generator: Union[callable, None] = None,
+    ):
+        if name is None:
+            name = assumption.__name__
+        if description is None:
+            description = ""
+        if uses is None:
+            uses = tuple(signature(assumption).parameters.keys())
+        if metadata is None:
+            metadata = {}
+        if doc_generator is None:
+            doc_generator = assumption_doc_generator
+        return cls(
+            name=name,
+            description=description,
+            uses=uses,
+            assumption=assumption,
+            metadata=metadata,
+            doc_generator=doc_generator,
+        )
+
+    def __call__(self, *args, **kwargs):
+        return self.assumption(*args, **kwargs)
 
 
 @attrs(frozen=True, slots=True)
-class AssumptionSet:
-    """Base class representing an assumption set."""
+class AssumptionSetRegistry:
+    """Class representing an assumption set registry."""
 
-    name = attrib(type=str, validator=instance_of(str))
     description = attrib(type=str, validator=optional(instance_of(str)))
-    registry = attrib(factory=dict, init=False, repr=False)
+    registry = attrib(factory=dict, init=False)
 
-    def get(self, assumption: str):
-        """Get an assumption."""
-
-        return self.registry[assumption]
-
-    def register(self, assumption: callable):
+    def register(
+        self,
+        assumption: callable = None,
+        *,
+        name: Union[str, None] = None,
+        description: Union[str, None] = None,
+        uses: Union[list, None] = None,
+        cache: Union[tuple, None] = None,
+        metadata: Union[dict, None] = None,
+        doc_generator: callable = None,
+    ):
         """Register an assumption."""
-
-        self.registry[assumption.__name__] = assumption
-
-    # def __doc__(self):
-    #     # doc = ""
-    #     return str(self)
+        if assumption is None:
+            return partial(
+                self.register,
+                name=name,
+                description=description,
+                uses=uses,
+                metadata=metadata,
+                doc_generator=doc_generator,
+            )
+        self.registry[assumption.__name__] = Assumption.create(
+            name=name,
+            description=description,
+            uses=uses,
+            assumption=assumption,
+            metadata=metadata,
+            doc_generator=doc_generator,
+        )
+        return assumption
 
 
 def def_assumption_set(description=None):
     """Define a set withan an assumption registry."""
 
-    return partial(AssumptionSet, description=description)
+    return AssumptionSetRegistry(description=description)
+
+
+def make_assumption_set_doc(name, description, registry):
+    if description is not None:
+        doc = f"{description}\n\n"
+    else:
+        doc = ""
+    doc += ".. rubric:: Assumptions\n\n"
+    for k, v in registry.items():
+        lines = v.__doc__.split("\n")
+        doc += lines[0] + "\n"
+        for line in lines[1:]:
+            doc += "\t" + line + "\n"
+        doc += "\n"
+    return doc
+
+
+@attrs(frozen=True, slots=True)
+class AssumptionSet:
+    """Base class representing a temporary assumption set."""
+
+    def get(self, assumption: callable, *args, **kwargs):
+        """Register an assumption."""
+
+        return getattr(self, assumption)(*args, **kwargs)
+
+
+def make_assumption_set(name: str, description: str, registry: dict):
+    """Make an assumption set. That is a child of the parent AssumptionSet."""
+    cls = make_class(name=name, attrs={}, bases=(AssumptionSet,), slots=True, frozen=True)
+    for asn_nm, asn_val in registry.items():
+        setattr(cls, asn_nm, staticmethod(asn_val))
+    cls.__doc__ = make_assumption_set_doc(name, description, registry)
+    return cls()
 
 
 @attrs(frozen=True, slots=True)
 class AssumptionRegistry:
     """Base class representing an assumption repository."""
 
-    def get(self, assumption_set: str, assumption: str):
+    def get(self, assumption_set: str, assumption: str, *args, **kwargs):
         """Get an assumption from an assumption set."""
 
-        return getattr(self, assumption_set).get(assumption)
+        return getattr(self, assumption_set).get(assumption, *args, **kwargs)
 
-    def register(self, assumption_set: Union[str, Tuple[str]]):
-        """Register an assumption to an assumption_set."""
 
-        def inner(assumption):
-            if isinstance(assumption_set, str):
-                getattr(self, assumption_set).register(assumption)
-            elif isinstance(assumption_set, tuple):
-                for a in assumption_set:
-                    getattr(self, a).register(assumption)
-            else:
-                msg = "The assumption_set must be type str or tuple not "
-                msg += f"{str(type(assumption_set))}."
-                raise ValueError(msg)
+def _make_registry_doc(summary: str, assumption_sets: Dict[str, AssumptionSet]):
+    if summary is None:
+        doc = ""
+    else:
+        doc = summary + "\n\n"
+    doc += ".. rubric:: Assumption Sets\n\n"
 
-        return inner
+    # doc += "Assumption Sets\n"
+    # doc += "---------------\n"
+    for v in assumption_sets.values():
+        doc += v.__doc__
+        doc += "\n\n"
+
+    return doc
 
 
 def assumption_registry(cls):
@@ -65,22 +187,25 @@ def assumption_registry(cls):
 
     def make_assumption_registry(cls):
 
-        attrs = {
-            k: attrib(default=v(name=k), init=False, repr=False)
+        assumption_sets = {
+            k: make_assumption_set(name=k, description=v.description, registry=v.registry)
             for k, v in cls.__dict__.items()
-            if isinstance(v, partial)
+            if isinstance(v, AssumptionSetRegistry)
         }
+        summary = cls.__doc__
         cls = make_class(
             cls.__name__,
-            attrs,
+            list(assumption_sets.keys()),
             bases=(AssumptionRegistry,),
             slots=True,
             frozen=True,
             repr=False,
         )
-        return cls
+        cls.__doc__ = _make_registry_doc(summary, assumption_sets)
+        cls.__repr__ = f"{cls.__name__}({', '.join(assumption_sets.keys())})"
+        return cls(**assumption_sets)  # note an instance is returned
 
     if cls is None:
         return partial(assumption_registry)
 
-    return make_assumption_registry(cls)()  # note an instance is returned
+    return make_assumption_registry(cls)
--- tests/test_assumption_registry.py
+++ tests/test_assumption_registry.py
@@ -1,20 +1,87 @@
-import pytest
+from inspect import signature
 
 from footings.assumption_registry import (
+    Assumption,
     AssumptionRegistry,
     AssumptionSet,
+    AssumptionSetRegistry,
     assumption_registry,
     def_assumption_set,
+    make_assumption_set,
 )
 
 
-class TestAssumptionSet:
+def func(x, y):
+    """This is func."""
+    return x + y
 
-    assumption_set = AssumptionSet(name="Test", description="This is a test.")
+
+class TestAssumption:
+
+    asn_1 = Assumption.create(name="Assumption1", assumption=func)
+    asn_2 = Assumption.create(
+        name="Assumption2", description="This is assumption 2.", assumption=func
+    )
+    asn_3 = Assumption.create(
+        name="Assumption3", description="This is assumption 3.", uses=(), assumption=func,
+    )
+    asn_4 = Assumption.create(
+        name="Assumption4",
+        description="This is assumption 4.",
+        assumption=func,
+        metadata={"x": "This is x.", "y": "This is y."},
+    )
+    asn_5 = Assumption.create(
+        name="Assumption5", assumption=func, doc_generator=lambda self: ""
+    )
 
     def test_init(self):
-        assert isinstance(self.assumption_set, AssumptionSet)
-        assert self.assumption_set.name == "Test"
+        assert isinstance(self.asn_1, Assumption)
+        assert isinstance(self.asn_2, Assumption)
+        assert isinstance(self.asn_3, Assumption)
+        assert isinstance(self.asn_4, Assumption)
+        assert isinstance(self.asn_5, Assumption)
+
+    def test_signature(self):
+        assert signature(self.asn_1) == signature(func)
+        assert signature(self.asn_2) == signature(func)
+        assert signature(self.asn_3) == signature(func)
+        assert signature(self.asn_4) == signature(func)
+        assert signature(self.asn_5) == signature(func)
+
+    def test_doc(self):
+        assert (
+            self.asn_1.__doc__
+            == "**Assumption1 [get('func', *args, **kwargs)] :**\nThis is func.\n\n"
+        )
+        assert (
+            self.asn_2.__doc__
+            == "**Assumption2 [get('func', *args, **kwargs)] :**\nThis is assumption 2.\n\n"
+        )
+        assert (
+            self.asn_3.__doc__
+            == "**Assumption3 [get('func', *args, **kwargs)] :**\nThis is assumption 3.\n\n"
+        )
+        assert (
+            self.asn_4.__doc__
+            == "**Assumption4 [get('func', *args, **kwargs)] :**\nThis is assumption 4.\n\n"
+        )
+        assert self.asn_5.__doc__ == ""
+
+    def test_call(self):
+        assert self.asn_1(x=1, y=2) == 3
+        assert self.asn_2(x=1, y=2) == 3
+        assert self.asn_3(x=1, y=2) == 3
+        assert self.asn_4(x=1, y=2) == 3
+        assert self.asn_5(x=1, y=2) == 3
+
+
+class TestAssumptionSetRegistry:
+
+    assumption_set = AssumptionSetRegistry(description="This is a test.")
+
+    def test_init(self):
+        assert isinstance(self.assumption_set, AssumptionSetRegistry)
         assert self.assumption_set.description == "This is a test."
         assert self.assumption_set.registry == {}
 
@@ -30,10 +97,6 @@ class TestAssumptionSet:
 
         assert list(self.assumption_set.registry.keys()) == ["func_1", "func_2"]
 
-    def test_get(self):
-        assert self.assumption_set.get("func_1")(1) == 2
-        assert self.assumption_set.get("func_2")(1) == 3
-
     def test_attributes(self):
         pass
 
@@ -41,11 +104,53 @@ class TestAssumptionSet:
         pass
 
 
+class TestAssumptionSet:
+
+    registry = {
+        "asn_1": Assumption.create(name="Assumption1", assumption=func),
+        "asn_2": Assumption.create(name="Assumption2", assumption=func),
+    }
+    asn_set_1 = make_assumption_set(
+        name="AssumptionSet1", description="This is AssumptionSet1.", registry=registry
+    )
+
+    def test_init(self):
+        assert isinstance(self.asn_set_1, AssumptionSet)
+        assert isinstance(self.asn_set_1.asn_1, Assumption)
+        assert isinstance(self.asn_set_1.asn_2, Assumption)
+
+    def test_get(self):
+        assert self.asn_set_1.get("asn_1", x=1, y=2) == 3
+        assert self.asn_set_1.get("asn_2", x=1, y=2) == 3
+
+    # def test_doc(self):
+    #     print(self.asn_set_1.__doc__)
+    #     assert 0
+
+
 class TestAssumptionRegistry:
     @assumption_registry
     class Registry:
+        """Test Registry."""
+
         One = def_assumption_set(description="Assumption Set One.")
         Two = def_assumption_set(description="Assumption Set Two.")
+
+        @One.register
+        def func_1(x):
+            """This is func1."""
+            return x + 1
+
+        @Two.register(name="Function 2")
+        def func_2(x):
+            """This is func2."""
+            return x + 2
+
+        @One.register(name="Function 3")
+        @Two.register(name="Function 3")
+        def func_3(x):
+            """This is func3."""
+            return x + 3
 
     def test_init(self):
         assert isinstance(self.Registry, AssumptionRegistry)
@@ -53,39 +158,41 @@ class TestAssumptionRegistry:
         assert isinstance(self.Registry.Two, AssumptionSet)
 
     def test_register(self):
-        @self.Registry.One.register
-        def func_1(x):
-            return x + 1
-
-        @self.Registry.register("Two")
-        def func_2(x):
-            return x + 2
-
-        @self.Registry.register(("One", "Two"))
-        def func_3(x):
-            return x + 3
-
-        assert list(self.Registry.One.registry.keys()) == ["func_1", "func_3"]
-        assert list(self.Registry.Two.registry.keys()) == ["func_2", "func_3"]
-
-        with pytest.raises(ValueError):
-
-            @self.Registry.register(["One", "Two"])
-            def func_4(x):
-                return x + 4
+        assert isinstance(self.Registry.One.func_1, Assumption)
+        assert isinstance(self.Registry.One.func_3, Assumption)
+        assert isinstance(self.Registry.Two.func_2, Assumption)
+        assert isinstance(self.Registry.Two.func_3, Assumption)
 
     def test_get(self):
-        assert self.Registry.get("One", "func_1")(1) == 2
-        assert self.Registry.One.get("func_1")(1) == 2
-        assert self.Registry.get("One", "func_3")(1) == 4
-        assert self.Registry.One.get("func_3")(1) == 4
-        assert self.Registry.get("Two", "func_2")(1) == 3
-        assert self.Registry.Two.get("func_2")(1) == 3
-        assert self.Registry.get("Two", "func_3")(1) == 4
-        assert self.Registry.Two.get("func_3")(1) == 4
+        assert self.Registry.get("One", "func_1", 1) == 2
+        assert self.Registry.One.get("func_1", 1) == 2
+        assert self.Registry.get("One", "func_3", 1) == 4
+        assert self.Registry.One.get("func_3", 1) == 4
+        assert self.Registry.get("Two", "func_2", 1) == 3
+        assert self.Registry.Two.get("func_2", 1) == 3
+        assert self.Registry.get("Two", "func_3", 1) == 4
+        assert self.Registry.Two.get("func_3", 1) == 4
 
     def test_attributes(self):
         pass
 
-    def test_doc(self):
-        pass
+    # def test_doc(self):
+    #     print(self.Registry.__doc__)
+    #     assert 0
+    #     doc = [
+    #         "Test Registry.\n",
+    #         "\n",
+    #         "Assumption Sets\n",
+    #         "---------------\n",
+    #         "One : Assumption Set One.\n",
+    #         "\tfunc_1(x)\n",
+    #         "\t\tThis is func1.\n",
+    #         "\tfunc_3(x)\n",
+    #         "\t\tThis is func3.\n",
+    #         "Two : Assumption Set Two.\n",
+    #         "\tfunc_2(x)\n",
+    #         "\t\tThis is func2.\n",
+    #         "\tfunc_3(x)\n",
+    #         "\t\tThis is func3.\n",
+    #     ]
+    #     assert self.Registry.__doc__ == "".join(doc)
