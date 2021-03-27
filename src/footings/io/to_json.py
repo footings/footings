f05a6fda09618afe543bd9a98616aa449639ed55
Dustin Tindall <dustin.tindall@gmail.com>
2021-03-27 07:18:05 -0500

    Pandas series are now wide vs indented and long.

--- src/footings/io/to_json.py
+++ src/footings/io/to_json.py
@@ -6,8 +6,6 @@ from inspect import isclass
 import numpy as np
 import pandas as pd
 
-# from ..exceptions import Error
-
 
 def _set_key(k):
     if type(k) not in [str, int, float, bool, None]:
@@ -15,19 +13,18 @@ def _set_key(k):
     return k
 
 
-def _column_to_list(col):
+def _column_to_json(col):
     def _set_val(val):
         if isinstance(val, (int, float, bool)):
             if math.isnan(val):
                 return None
-            else:
-                return val
+            return val
         return val
 
     col = col.to_list()
     if any(math.isnan(val) for val in col if isinstance(val, (int, float, bool))):
         col = [_set_val(val) for val in col]
-    return col
+    return json.dumps(col)
 
 
 class AuditJSONEncoder(json.JSONEncoder):
@@ -41,13 +38,11 @@ class AuditJSONEncoder(json.JSONEncoder):
         elif isinstance(obj, np.generic):
             return obj.item()
         elif isinstance(obj, pd.DataFrame):
-            return {col: _column_to_list(obj[col]) for col in obj.columns}
+            return {col: _column_to_json(obj[col]) for col in obj.columns}
         elif isinstance(obj, pd.Series):
-            return {obj.name: _column_to_list(obj)}
+            return {obj.name: _column_to_json(obj)}
         elif isinstance(obj, (pd.Timestamp, date, datetime)):
             return str(obj)
-        # elif isinstance(obj, Error):
-        #     return obj.to_audit_json()
         elif callable(obj):
             return "callable: " + obj.__module__ + "." + obj.__qualname__
         elif isclass(obj) and issubclass(obj, Exception):
@@ -58,4 +53,4 @@ class AuditJSONEncoder(json.JSONEncoder):
 
 def create_footings_json_file(d, file, **kwargs):
     with open(file, "w") as data_file:
-        json.dump(obj=d, fp=data_file, cls=AuditJSONEncoder, indent=2, **kwargs)
+        json.dump(obj=d, fp=data_file, indent=2, cls=AuditJSONEncoder, **kwargs)
